e6a84e377da4d9e78edbe5e5d4c85a97c03108b9
Lijin Syam K <lijinshyam@gmail.com>
2011-07-06 12:45:45 +0530

    Removed the unwanted Source Files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,18 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Default configuration
+CND_PLATFORM_Default=GNU-Linux-x86
+CND_ARTIFACT_DIR_Default=
+CND_ARTIFACT_NAME_Default=
+CND_ARTIFACT_PATH_Default=
+CND_PACKAGE_DIR_Default=dist/Default/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Default=myplugin.tar
+CND_PACKAGE_PATH_Default=dist/Default/GNU-Linux-x86/package/myplugin.tar
+CND_SYSINCLUDES_C_Default=/usr/local/include,/usr/lib/gcc/i686-linux-gnu/4.4.5/include,/usr/lib/gcc/i686-linux-gnu/4.4.5/include-fixed,/usr/include,/usr/local/include/gstreamer-0.10,/usr/include/libxml2,/usr/local/include/glib-2.0,/usr/include/linux
+CND_SYSINCLUDES_CPP_Default=/usr/include/c++/4.4,/usr/include/c++/4.4/i686-linux-gnu,/usr/include/c++/4.4/backward,/usr/local/include,/usr/lib/gcc/i686-linux-gnu/4.4.5/include,/usr/lib/gcc/i686-linux-gnu/4.4.5/include-fixed,/usr/include
